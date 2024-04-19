073c4e925a9cbb6bd339a3c317dc640d60ece527
Ömer Ünal <49806742+kharalos@users.noreply.github.com>
2024-04-19 23:09:16 +0300

    walking animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerStats stats;
     [SerializeField] private CharacterController controller;
+    [SerializeField] private Animator animator;
     [SerializeField] private float initialSpeed = 5f;
     [SerializeField] private float maxPowerSpeed = 12f;
     [SerializeField] private float rotationSpeed = 90f;
+    private static readonly int Walking = Animator.StringToHash("walking");
 
-    
+
     void Update()
     {
         controller.Move(Physics.gravity * Time.deltaTime);
@@ -19,9 +21,11 @@ public class PlayerController : MonoBehaviour
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
+
             Vector3 move = transform.forward * z;
 
             var powerSpeedMultiplier = Mathf.InverseLerp(0f, stats.MaxHealth, stats.CurrentHealth);
+            animator.SetFloat(Walking, move.magnitude * (powerSpeedMultiplier + 1));
             var speed = initialSpeed + maxPowerSpeed * powerSpeedMultiplier;
             controller.SimpleMove(move * speed);
             
